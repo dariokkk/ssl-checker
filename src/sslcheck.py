c22046de210a762fb879670888ee55de1a5776fa
Dario Kuceki Knopfholz <dario@celepar.pr.gov.br>
2026-06-25 07:55:54 -0300

    Calcula dias restantes

--- src/sslcheck.py
+++ src/sslcheck.py
@@ -1,6 +1,9 @@
 import ssl
 import socket
 
+from datetime import datetime
+
+
 host = "google.com"
 port = 443
 
@@ -16,4 +19,14 @@ with ctx.wrap_socket(
 
     cert = conn.getpeercert()
 
-print(cert["notAfter"])
+data_expiracao = datetime.strptime(
+    cert["notAfter"],
+    "%b %d %H:%M:%S %Y %Z"
+)
+#agora_utc = datetime.now(timezone.utc)
+
+dias_restantes = (data_expiracao - datetime.utcnow()).days
+
+print(f"Host: {host}")
+print(f"Expira em: {data_expiracao}")
+print(f"Dias restantes: {dias_restantes}")
