1d13591328a85406339aa463645f5f8f4f78a005
Dario Kuceki Knopfholz <dario@celepar.pr.gov.br>
2026-06-26 08:36:40 -0300

    Corrige timezone e recebe URL como parâmetro

--- src/sslcheck.py
+++ src/sslcheck.py
@@ -1,32 +1,52 @@
+import argparse
 import ssl
 import socket
 
-from datetime import datetime
+from datetime import datetime, UTC
 
-
-host = "google.com"
 port = 443
 
-ctx = ssl.create_default_context()
+def consultar_certificado(host, port=443):
 
-with ctx.wrap_socket(
-    socket.socket(),
-    server_hostname=host
-) as conn:
+    contexto = ssl.create_default_context()
 
-    conn.settimeout(5)
-    conn.connect((host, port))
+    with contexto.wrap_socket(
+        socket.socket(),
+        server_hostname=host
+    ) as conexao:
 
-    cert = conn.getpeercert()
+        conexao.settimeout(5)
+        conexao.connect((host, port))
 
-data_expiracao = datetime.strptime(
-    cert["notAfter"],
-    "%b %d %H:%M:%S %Y %Z"
-)
-#agora_utc = datetime.now(timezone.utc)
+        return conexao.getpeercert()
 
-dias_restantes = (data_expiracao - datetime.utcnow()).days
 
-print(f"Host: {host}")
-print(f"Expira em: {data_expiracao}")
-print(f"Dias restantes: {dias_restantes}")
+def main():
+    parser = argparse.ArgumentParser(
+        description="Verificador de certificados SSL/TLS"
+    )
+    parser.add_argument(
+        "host",
+        help="Host a ser consultado"
+    )
+    args = parser.parse_args()
+    cert = consultar_certificado(args.host)
+
+    agora_utc = datetime.now(UTC)
+    agora_local = datetime.now().astimezone()
+    timezone_local = agora_local.tzinfo
+
+    data_expiracao_utc = datetime.strptime(
+        cert["notAfter"],
+        "%b %d %H:%M:%S %Y %Z"
+    ).replace(tzinfo=UTC)
+    data_expiracao_local = data_expiracao_utc.astimezone(timezone_local)
+
+    dias_restantes = (data_expiracao_utc - agora_utc).days
+
+    print(f"Host............: {args.host}")
+    print(f"Expira em.......: {data_expiracao_local}")
+    print(f"Dias restantes..: {dias_restantes}")
+
+if __name__ == "__main__":
+    main()
